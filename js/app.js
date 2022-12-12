3e59fe7e00d65b1a0fd82c44d4f197ac0e5edd80
Tal <trimoni8@gmail.com>
2022-12-12 10:56:08 -0500

    get el board

--- js/app.js
+++ js/app.js
@@ -15,9 +15,12 @@ function initialize(){
   // create the game board
   for (let r = 0; r < height; r++){
     for (let c = 0; c < width; c++){
+      // <span id='0-0' class='tile' ></span>
       let tile = document.createElement('span')
       tile.id = r.toString() + '-' + c.toString()
       tile.classList('tile')
+      tile.innerText = 'p'
+      document.getElementById('board').appendChild(tile)
     }
   }
 }
