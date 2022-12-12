738ad4786e0345eb3398f62dc81588a0c330dab2
Tal <trimoni8@gmail.com>
2022-12-12 10:54:46 -0500

    tile id

--- js/app.js
+++ js/app.js
@@ -15,7 +15,9 @@ function initialize(){
   // create the game board
   for (let r = 0; r < height; r++){
     for (let c = 0; c < width; c++){
-      
+      let tile = document.createElement('span')
+      tile.id = r.toString() + '-' + c.toString()
+      tile.classList('tile')
     }
   }
 }
