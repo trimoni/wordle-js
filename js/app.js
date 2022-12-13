d15e7570a63986958e80a103aeac3fc2c8e17635
Tal <trimoni8@gmail.com>
2022-12-13 12:33:43 -0500

    keyboard stub

--- js/app.js
+++ js/app.js
@@ -36,7 +36,13 @@ function initialize() {
     ["Q", "W", "E", "R", "T", "Y", "U", "I", "O", "P"],
     ["A", "S", "D", "F", "G", "H", "J", "K", "L", " "],
     ["Enter", "Z", "X", "C", "V", "B", "N", "M", "⌫" ]
-]
+  ]
+
+  for (let i = 0; i < keyboard.length; i++){
+    let currRow = keyboard[i]
+    let keyboardRow = document.createElement('div')
+    keyboard.classList.add('keyboard-row')
+  }
 
   document.addEventListener("keyup", (e) => {
     if (gameOver) return;
